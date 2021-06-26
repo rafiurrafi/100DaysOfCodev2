7c9f1b53de0cac4b85a61cd5e61b9e73b76489fe
Rafiur Rahman <rrfoundary@gmail.com>
2021-06-26 17:55:23 +0600

    Removed Form wave animation

--- Day 7 - Form wave animation/main.js
+++
@@ -1,8 +0,0 @@
-const labels = document.querySelectorAll(".form-control label");
-
-labels.forEach((label) => {
-  label.innerHTML = label.innerText
-    .split("")
-    .map((l, i) => `<span style = "transition-delay : ${i * 50}ms">${l}</span>`)
-    .join("");
-});
