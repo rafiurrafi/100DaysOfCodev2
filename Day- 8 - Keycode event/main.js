7418cc94d2667a78dc79a24890f634751f1cc2bd
Rafiur Rahman <rrfoundary@gmail.com>
2021-06-27 12:10:19 +0600

    KEvt - Complete this project

--- Day- 8 - Keycode event/main.js
+++ Day- 8 - Keycode event/main.js
@@ -1,3 +1,8 @@
-document.addEventListener("keyup", (e) => {
-  console.log(e);
+const key = document.querySelector("#key");
+const keyCode = document.querySelector("#keyCode");
+const code = document.querySelector("#code");
+window.addEventListener("keyup", (e) => {
+  key.innerHTML = e.key;
+  keyCode.innerHTML = e.keyCode;
+  code.innerHTML = e.code;
 });
