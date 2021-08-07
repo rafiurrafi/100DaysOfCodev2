6a2942d905d8da066a3a873cb16dfd35d602f312
Rafiur Rahman <rrfoundary@gmail.com>
2021-08-07 13:07:57 +0600

    airshift - added handling quantity functionlity

--- Day- Ex5 - Airshift/js/main.js
+++ Day- Ex5 - Airshift/js/main.js
@@ -39,4 +39,18 @@ $(document).ready(function () {
       ".section-tracking-download .iti.iti--allow-dropdown .form-control"
     )
     .classList.add("w-100");
-});
+}); // End of document ready
+
+// .................................
+// quantity handling in cart page
+// .................................
+function handleProductQuantity(productId, operation) {
+  const product = document.querySelector("#" + productId);
+  if (operation == "inc") {
+    let value = +product.innerText++;
+    console.log(value);
+  } else {
+    let value = +product.innerText--;
+    console.log(value);
+  }
+}
