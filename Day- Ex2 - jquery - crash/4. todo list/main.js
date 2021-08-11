f2a931f5f04c10c9496ba9cfd5600a8c8eecdc6e
Rafiur Rahman <rrfoundary@gmail.com>
2021-08-11 10:33:12 +0600

    TDL - fixed item not found problem

--- Day- Ex2 - jquery - crash/4. todo list/main.js
+++ Day- Ex2 - jquery - crash/4. todo list/main.js
@@ -1,22 +1,27 @@
 const items = [
   { _id: 1, title: "Feed Cow" },
-  { _id: 1, title: "Milk Cow" },
-  { _id: 1, title: "Bathe Cow" },
-  { _id: 1, title: "Sleep Cow" },
+  { _id: 2, title: "Milk Cow" },
+  { _id: 3, title: "Bathe Cow" },
+  { _id: 4, title: "Sleep Cow" },
 ];
 
 // decare variables
 const itemContainer = document.querySelector(".todo-list__items");
 items.map((item) => {
-  addItem(item);
+  displayItem(item);
 });
-function addItem(item) {
+function displayItem(item) {
   const singleItemContainer = document.createElement("div");
   singleItemContainer.className = "todo-list__item px-40";
-  singleItemContainer.innerHTML = ` <div class="todo-list__item-text">Feed cow</div>
-  <div class="todo-list__item-actions">
-    <a href="javascript:void(0)"><i class="fa fa-pencil"></i></a>
-    <a href="javascript:void(0)"><i class="fa fa-trash"></i></a>
+  singleItemContainer.innerHTML = ` <div class="todo-list__item-text">${item.title}</div>
+  <div class="todo-list__item-actions"> 
+    <a href="javascript:void(0)" id = "${item._id}" onclick = "editItem('${item.title}', this)" ><i class="fa fa-pencil"></i></a>
+    <a href="javascript:void(0)" id = "${item._id}"><i class="fa fa-trash"></i></a>
   </div>`;
   itemContainer.append(singleItemContainer);
+}
+
+// Edit item function
+function editItem(item, evt) {
+  console.log(evt.parentElement.parentElement);
 }
