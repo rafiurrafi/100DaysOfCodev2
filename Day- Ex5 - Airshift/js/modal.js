20fa16701c9a31441976a4937391a0141ffe8eb7
Rafiur Rahman <rrfoundary@gmail.com>
2021-08-12 23:27:31 +0600

    airshift - fixed modal input button inline problem

--- Day- Ex5 - Airshift/js/modal.js
+++ Day- Ex5 - Airshift/js/modal.js
@@ -41,10 +41,10 @@ $(document).ready(function () {
     separateDialCode: true,
     utilsScript: "js/utils.js",
   });
-  document.querySelector(".iti.iti--allow-dropdown").classList.add("w-75");
-  document
-    .querySelector(".iti.iti--allow-dropdown .form-control")
-    .classList.add("w-100");
+  // document.querySelector(".iti.iti--allow-dropdown").classList.add("w-75");
+  // document
+  //   .querySelector(".iti.iti--allow-dropdown .form-control")
+  //   .classList.add("w-100");
   // Tracking page
   var input = document.querySelector("#telephone-track");
   intlTelInput(input, {
@@ -59,12 +59,12 @@ $(document).ready(function () {
     separateDialCode: true,
     utilsScript: "js/utils.js",
   });
-  document
-    .querySelector(".section-tracking-download .iti.iti--allow-dropdown")
-    .classList.add("w-65");
-  document
-    .querySelector(
-      ".section-tracking-download .iti.iti--allow-dropdown .form-control"
-    )
-    .classList.add("w-100");
+  // document
+  //   .querySelector(".section-tracking-download .iti.iti--allow-dropdown")
+  //   .classList.add("w-65");
+  // document
+  //   .querySelector(
+  //     ".section-tracking-download .iti.iti--allow-dropdown .form-control"
+  //   )
+  //   .classList.add("w-100");
 }); // End of document ready
