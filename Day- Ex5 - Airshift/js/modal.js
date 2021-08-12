ddadf71210d03e3ff70dd944b63bc446e3206414
Rafiur Rahman <rrfoundary@gmail.com>
2021-08-12 22:46:48 +0600

    airshift - added close modal

--- Day- Ex5 - Airshift/js/modal.js
+++ Day- Ex5 - Airshift/js/modal.js
@@ -1,10 +1,13 @@
 const modal = document.querySelector(".section-modal");
 const openModalBtn = document.querySelectorAll(".slider-btn");
-// const modalCloseBtn
+const modalCloseBtn = document.querySelector(".modal-content__close");
 
 for (let i = 0; i < openModalBtn.length; i++) {
   openModalBtn[i].addEventListener("click", () => {
     modal.style.display = "block";
   });
 }
+modalCloseBtn.addEventListener("click", () => {
+  modal.style.display = "none";
+});
 console.log(openModalBtn);
