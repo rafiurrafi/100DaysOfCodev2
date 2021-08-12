b79d91ba02c540bf901a610f89574f9479e2f00a
Rafiur Rahman <rrfoundary@gmail.com>
2021-08-12 23:13:56 +0600

    airshift - specific downalod app class

--- Day- Ex5 - Airshift/js/modal.js
+++ Day- Ex5 - Airshift/js/modal.js
@@ -24,3 +24,47 @@ function openModal() {
 function closeModal() {
   modal.style.display = "none";
 }
+
+// Phone number in modal
+$(document).ready(function () {
+  // Vanilla Javascript
+  var input = document.querySelector("#modal-telephone");
+  intlTelInput(input, {
+    // options here
+    initialCountry: "bd",
+    customPlaceholder: function (
+      selectedCountryPlaceholder,
+      selectedCountryData
+    ) {
+      return "";
+    },
+    separateDialCode: true,
+    utilsScript: "js/utils.js",
+  });
+  document.querySelector(".iti.iti--allow-dropdown").classList.add("w-75");
+  document
+    .querySelector(".iti.iti--allow-dropdown .form-control")
+    .classList.add("w-100");
+  // Tracking page
+  var input = document.querySelector("#telephone-track");
+  intlTelInput(input, {
+    // options here
+    initialCountry: "bd",
+    customPlaceholder: function (
+      selectedCountryPlaceholder,
+      selectedCountryData
+    ) {
+      return "";
+    },
+    separateDialCode: true,
+    utilsScript: "js/utils.js",
+  });
+  document
+    .querySelector(".section-tracking-download .iti.iti--allow-dropdown")
+    .classList.add("w-65");
+  document
+    .querySelector(
+      ".section-tracking-download .iti.iti--allow-dropdown .form-control"
+    )
+    .classList.add("w-100");
+}); // End of document ready
