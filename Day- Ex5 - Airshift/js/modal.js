36ec132f4fc0f4044063555c624b1db13d47121e
Rafiur Rahman <rrfoundary@gmail.com>
2021-08-12 22:44:09 +0600

    airshift - added open modal functionality

--- Day- Ex5 - Airshift/js/modal.js
+++ Day- Ex5 - Airshift/js/modal.js
@@ -1,9 +1,10 @@
 const modal = document.querySelector(".section-modal");
 const openModalBtn = document.querySelectorAll(".slider-btn");
+// const modalCloseBtn
 
 for (let i = 0; i < openModalBtn.length; i++) {
   openModalBtn[i].addEventListener("click", () => {
-    console.log("Open modal");
+    modal.style.display = "block";
   });
 }
 console.log(openModalBtn);
