592460fada70b7098d031e15583092f49a5eb4fd
Rafiur Rahman <rrfoundary@gmail.com>
2021-08-12 22:04:34 +0600

    airshift - added declared variables

--- Day- Ex5 - Airshift/js/modal.js
+++ Day- Ex5 - Airshift/js/modal.js
@@ -1,2 +1,9 @@
 const modal = document.querySelector(".section-modal");
-console.log(modal);
+const openModalBtn = document.querySelectorAll(".slider-btn");
+
+for (let i = 0; i < openModalBtn.length; i++) {
+  openModalBtn[i].addEventListener("click", () => {
+    console.log("Open modal");
+  });
+}
+console.log(openModalBtn);
