f58e0497859494c3c1b6fe80ecb9f736011990bc
Rafiur Rahman <rrfoundary@gmail.com>
2021-08-25 14:57:03 +0600

    slider - added clone node

--- Day-11 - sliders/slide_1/main.js
+++ Day-11 - sliders/slide_1/main.js
@@ -1,7 +1,14 @@
 const rep = document.querySelectorAll(".copyMe");
-
+const holder = {};
 rep.forEach((element, index) => {
   element.addEventListener("click", function () {
-    console.log(this);
+    holder.obj = element.cloneNode(true);
+    holder.obj.classList.add("newBorn");
+    holder.obj.style.cursor = "move";
+    holder.obj.style.left = element.offsetLeft + "px";
+    holder.obj.style.top = element.offsetTop + 200 + "px";
+    document.body.append(holder.obj);
+    console.log(holder.obj);
   });
+  element.style.left = index * 120 + "px";
 });
