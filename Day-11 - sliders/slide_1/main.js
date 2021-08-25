e94a5b3734d5d66de57c1e765b8df86a534f34e9
Rafiur Rahman <rrfoundary@gmail.com>
2021-08-25 20:25:49 +0600

    slider - added move animation

--- Day-11 - sliders/slide_1/main.js
+++ Day-11 - sliders/slide_1/main.js
@@ -4,9 +4,25 @@ boxs.forEach((box) => {
   box.addEventListener("click", function (e) {
     holder.obj = box.cloneNode(true);
     holder.obj.classList.add("copied-box");
-    holder.obj.style.left = this.offsetLeft + "px";
-    holder.obj.style.top = this.offsetTop + 200 + "px";
-    console.log(e.clientX);
+    // holder.obj.style.left = this.offsetLeft + "px";
+    // holder.obj.style.top = this.offsetTop + 200 + "px";
+
+    //animation box falling
+    holder.obj.startX = box.offsetLeft;
+    holder.obj.startY = box.offsetTop;
+    holder.obj.moves = Math.floor(Math.random() * 25);
+    holder.obj.interval = setInterval(moveBox, 20);
+    function moveBox(e) {
+      if (holder.obj.moves <= 0) {
+        clearInterval(holder.obj.interval);
+      } else {
+        holder.obj.moves--;
+        holder.obj.startX += 1;
+        holder.obj.startY += 10;
+        holder.obj.style.left = holder.obj.startX + "px";
+        holder.obj.style.top = holder.obj.startY + "px";
+      }
+    }
     //append to body
     document.body.append(holder.obj);
     console.log(holder.obj);
