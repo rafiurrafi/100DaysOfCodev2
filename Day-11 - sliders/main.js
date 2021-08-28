9f90debbeb67142093fb5a722f360a49a3e47ae5
Rafiur Rahman <rrfoundary@gmail.com>
2021-08-28 21:35:52 +0600

    slider - added basic move bottom class

--- Day-11 - sliders/main.js
+++ Day-11 - sliders/main.js
@@ -1,3 +1,15 @@
 const mainNumber = document.querySelector(".slider__number-main");
+const sliderNumbers = document.querySelector(".slider__numbers");
+console.log(sliderNumbers);
 console.log(mainNumber);
-function clone() {}
+function clone() {
+  const cloneNumber = mainNumber.cloneNode(true);
+  cloneNumber.classList.add("slider__number-clone");
+  cloneNumber.classList.add("move-to-bottom");
+  sliderNumbers.appendChild(cloneNumber);
+  setTimeout(() => {
+    sliderNumbers.removeChild(cloneNumber);
+    console.log(cloneNumber);
+  }, 500);
+  console.log(sliderNumbers);
+}
