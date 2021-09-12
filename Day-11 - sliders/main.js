813718d561fc0ae01963861a07f94fd773444d86
Rafiur Rahman <rrfoundary@gmail.com>
2021-09-12 11:17:12 +0600

    slider - add tree class

--- Day-11 - sliders/main.js
+++ Day-11 - sliders/main.js
@@ -1,17 +1,21 @@
-const mainNumber = document.querySelector(".slider__number-main");
-const sliderNumbers = document.querySelector(".slider__numbers");
-let count = 0;
-function clone() {
-  const cloneNumber = mainNumber.cloneNode(true);
-  cloneNumber.classList.add("slider__number-clone");
-  if (!(count % 2)) cloneNumber.classList.add("move-to-right-bottom");
-  else cloneNumber.classList.add("move-to-bottom");
-  sliderNumbers.appendChild(cloneNumber);
-  setTimeout(() => {
-    // sliderNumbers.removeChild(cloneNumber);
-    cloneNumber.remove();
-    console.log(cloneNumber);
-  }, 600);
-  console.log(sliderNumbers);
-  count++;
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
 }
+class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
+  insert(value) {
+    var newNode = new Node(value);
+    if (!this.root) {
+      this.root = newNode;
+      return this;
+    }
+  }
+}
+var tree = new BinarySearchTree();
+tree.root = new Node(10);
