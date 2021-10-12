a4cca1471d55e9f82be09c5d46d9a01c2209bd2e
Rafiur Rahman <rrfoundary@gmail.com>
2021-10-12 23:57:51 +0600

    filter - finalize the filter

--- Day-12 - FAQ/main.js
+++ Day-12 - FAQ/main.js
@@ -34,3 +34,42 @@ $("#fob").on("change", function () {
     $("#type").val("select");
   }
 });
+rage == "food") {
+    $('li').each(function(){
+       var item = $(this);
+       var data = item.data();
+          if(data.filter.food_or_beverage == "food") { 
+            item.show();
+          }
+    });
+ }
+ if(storage == "beverage") {
+    $('li').each(function(){ 
+       var item = $(this);
+       var data = item.data();
+          if(data.filter.food_or_beverage == "beverage") { 
+            item.show();
+          }
+    });
+ }
+
+$('li:visible').each(function () {
+  console.log('val');
+  var item_filter = $(this).data();
+  var item = $(this);
+  if(val == "fruit" && item_filter.filter.type != "fruit") {
+    item.hide();
+  }
+   if(val == "vegetable" && item_filter.filter.type != "vegetable") {
+    item.hide();
+  }
+   if(val == "juice" && item_filter.filter.type != "juice") {
+    item.hide();
+  }
+   if(val == "nojuice" && item_filter.filter.type != "nojuice") {
+    item.hide();
+  }
+ });
+}); 
+  
+  
