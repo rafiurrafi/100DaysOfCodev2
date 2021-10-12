7374df74449b3304e12625e63309e47886cc0fdd
Rafiur Rahman <rrfoundary@gmail.com>
2021-10-12 23:52:42 +0600

    filter - added show option to select field

--- Day-12 - FAQ/main.js
+++ Day-12 - FAQ/main.js
@@ -1,25 +1,36 @@
-localStorage.setItem('fob', 'select');
+localStorage.setItem("fob", "select");
 
-$('#fob').on('change', function() {
-   var val = $(this).val();
-   localStorage.setItem('fob', val);
-   $('select#type>option').each(function () {
-     var filter = $(this).data();
-     var elem = $(this);
-     if((filter.filter.food_or_beverage != val) && filter.filter.food_or_beverage !="")  {
-       elem.hide();
-     } else if(filter.filter.food_or_beverage !="") {
-       elem.show();
-     } else {
-       elem.show();
-     }
-   });
-   $('li').each(function () {
+$("#fob").on("change", function () {
+  var val = $(this).val();
+  localStorage.setItem("fob", val);
+  $("select#type>option").each(function () {
+    var filter = $(this).data();
+    var elem = $(this);
+    if (
+      filter.filter.food_or_beverage != val &&
+      filter.filter.food_or_beverage != ""
+    ) {
+      elem.hide();
+    } else if (filter.filter.food_or_beverage != "") {
+      elem.show();
+    } else {
+      elem.show();
+    }
+  });
+  $("li").each(function () {
     var item_filter = $(this).data();
     var item = $(this);
-    if(item_filter.filter.food_or_beverage != val && val != "select")  {
-     item.hide();
+    if (item_filter.filter.food_or_beverage != val && val != "select") {
+      item.hide();
     } else {
       item.show();
     }
   });
+  $("#type").val("select");
+  if (val == "select") {
+    $("select#type>option").each(function () {
+      $(this).show();
+    });
+    $("#type").val("select");
+  }
+});
