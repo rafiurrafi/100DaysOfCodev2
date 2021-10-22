5220e87eb0abb0991c308606eaf229732f66f1a4
Rafiur Rahman <rrfoundary@gmail.com>
2021-10-22 19:26:07 +0600

    form - refactor code into more readable

--- Day-13 - form-validator/main.js
+++ Day-13 - form-validator/main.js
@@ -5,17 +5,18 @@ const passwordInput = document.querySelector("#password");
 const passwordInput2 = document.querySelector("#password2");
 button.addEventListener("click", (e) => {
   e.preventDefault();
-  if (userInput.value == "") showError(userInput, "Username is Empty");
-  else showSuccess(userInput);
-  if (emailInput.value == "") showError(emailInput, "Email is Empty");
-  else if (!isValidateEmail(emailInput.value))
-    showError(emailInput, "Email is not valid");
-  else showSuccess(emailInput);
-  if (passwordInput.value == "") showError(passwordInput, "Password is Empty");
-  else showSuccess(passwordInput);
-  if (passwordInput2.value == "")
-    showError(passwordInput2, "Password is Empty");
-  else showSuccess(passwordInput2);
+  //   if (userInput.value == "") showError(userInput, "Username is Empty");
+  //   else showSuccess(userInput);
+  //   if (emailInput.value == "") showError(emailInput, "Email is Empty");
+  //   else if (!isValidateEmail(emailInput.value))
+  //     showError(emailInput, "Email is not valid");
+  //   else showSuccess(emailInput);
+  //   if (passwordInput.value == "") showError(passwordInput, "Password is Empty");
+  //   else showSuccess(passwordInput);
+  //   if (passwordInput2.value == "")
+  //     showError(passwordInput2, "Password is Empty");
+  //   else showSuccess(passwordInput2);
+  checkRequired([userInput, passwordInput, emailInput, passwordInput2]);
 });
 function showError(input, msg) {
   const formControl = input.parentNode;
@@ -31,6 +32,20 @@ function isValidateEmail(email) {
   const re =
     /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   return re.test(String(email).toLowerCase());
+}
+function checkRequired(inputs) {
+  inputs.forEach((input) => {
+    if (input.value == "") showError(input, getFieldName(input) + " is Empty");
+    else showSuccess(input);
+    //email
+    if (input.id == "email") {
+      if (!isValidateEmail(emailInput.value))
+        showError(emailInput, "Email is not valid");
+    }
+  });
+}
+function getFieldName(input) {
+  return input.id.charAt(0).toUpperCase() + input.id.slice(1);
 }
 // function isEmpty(input) {
 //   return !input.value.length;
