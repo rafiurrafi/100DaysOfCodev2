3f1a380cfaa26a9d998fed137eca98c00cccc9bc
Rafiur Rahman <rrfoundary@gmail.com>
2021-10-22 18:45:53 +0600

    form - added stylesheet to decorate

---
+++ Day-12 - FAQ/main.js
@@ -0,0 +1,13 @@
+// const incrementBtn = document.querySelector("button");
+// // const button = document.querySelector("#btn2");
+// const count = document.querySelector("h1");
+// let number = 0;
+
+// incrementBtn.addEventListener("click", function () {
+//   number++;
+//   count.textContent = number;
+// });
+// let number = 0;
+// const heading = document.createElement("h1");
+// heading.textContent = number;
+// document.body.appendChild(heading);
