db9dcbf90ec41cdb68512d10a6f7d22271c442c4
Rafiur Rahman <rrfoundary@gmail.com>
2021-10-23 20:55:02 +0600

    form - refactor code by cleaning some function

--- Day-13 - form-validator/main.js
+++ Day-13 - form-validator/main.js
@@ -3,47 +3,39 @@ const userInput = document.querySelector("#username");
 const emailInput = document.querySelector("#email");
 const passwordInput = document.querySelector("#password");
 const passwordInput2 = document.querySelector("#password2");
+
 button.addEventListener("click", (e) => {
   e.preventDefault();
-  //   if (userInput.value == "") showError(userInput, "Username is Empty");
-  //   else showSuccess(userInput);
-  //   if (emailInput.value == "") showError(emailInput, "Email is Empty");
-  //   else if (!isValidateEmail(emailInput.value))
-  //     showError(emailInput, "Email is not valid");
-  //   else showSuccess(emailInput);
-  //   if (passwordInput.value == "") showError(passwordInput, "Password is Empty");
-  //   else showSuccess(passwordInput);
-  //   if (passwordInput2.value == "")
-  //     showError(passwordInput2, "Password is Empty");
-  //   else showSuccess(passwordInput2);
-  checkRequired([userInput, passwordInput, emailInput, passwordInput2]);
+  // if (userInput.value == "") showError(userInput, "Username cannot be empty");
+  // else showSuccess(userInput);
+  // if (emailInput.value == "") showError(emailInput, "email cannot be empty");
+  // else showSuccess(emailInput);
+  // if (password.value == "") showError(password, "Password cannot be empty");
+  // else showSuccess(password);
+  // if (passwordInput2.value == "")
+  //   showError(passwordInput2, "password2 cannot be empty");
+  // else showSuccess(passwordInput2);
+  checkRequired([userInput, emailInput, passwordInput, passwordInput2]);
   checkLength(userInput, 3, 15);
-  checkLength(emailInput, 3, 15);
 });
+
 function showError(input, msg) {
-  const formControl = input.parentNode;
-  formControl.className = "form-control error";
-  const small = formControl.querySelector("small");
+  const parent = input.parentNode;
+  const small = parent.querySelector("small");
+  parent.className = "form-control error";
   small.textContent = msg;
+  console.log(parent);
 }
 function showSuccess(input) {
-  const formControl = input.parentNode;
-  formControl.className = "form-control success";
+  const parent = input.parentNode;
+  parent.className = "form-control success";
 }
-function isValidateEmail(email) {
-  const re =
-    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  return re.test(String(email).toLowerCase());
-}
+
 function checkRequired(inputs) {
-  inputs.forEach((input) => {
-    if (input.value == "") showError(input, getFieldName(input) + " is Empty");
+  inputs.map((input) => {
+    if (input.value.trim() == "")
+      showError(input, getFieldName(input) + " is empty");
     else showSuccess(input);
-    //email
-    if (input.id == "email") {
-      if (!isValidateEmail(emailInput.value))
-        showError(emailInput, "Email is not valid");
-    }
   });
 }
 function getFieldName(input) {
@@ -51,35 +43,14 @@ function getFieldName(input) {
 }
 function checkLength(input, min, max) {
   if (input.value.length < min)
-    showError(input, getFieldName(input) + " must be " + min + " characters");
+    showError(
+      input,
+      getFieldName(input) + " should at least " + min + " character"
+    );
   else if (input.value.length > max)
     showError(
       input,
-      getFieldName(input) + " must be less than " + max + " characters"
+      getFieldName(input) + " should less than " + max + " character"
     );
   else showSuccess(input);
 }
-// function isEmpty(input) {
-//   return !input.value.length;
-// }
-// function isMinimumLength(input, length) {
-//   if (input.value.length >= length) return true;
-//   return false;
-// }
-// function isPasswordMatch(password1, password2) {
-//   if (password1.value == password2.value) return true;
-//   else return false;
-// }
-// function validateUsername() {
-//   if (isEmpty(userInput)) {
-//     const userParent = userInput.parentNode;
-//     const small = document.querySelector("." + userParent.className + " small");
-//     small.innerHTML = "Username is empty";
-//     userParent.className = "form-control error";
-//   } else if (isMinimumLength(userInput, 6)) {
-//     const userParent = userInput.parentNode;
-//     const small = document.querySelector("." + userParent.className + " small");
-//     small.innerHTML = "Username is less than 6";
-//     userParent.className = "form-control error";
-//   }
-// }
