6d0b24ea5d41008d03bd0baa144f7e5b8ec8cef7
Rafiur Rahman <rrfoundary@gmail.com>
2021-10-25 21:20:51 +0600

    movie - added select seat functionality

---
+++ Day-14 - movie seat booking/main.js
@@ -0,0 +1,16 @@
+const selectMovie = document.querySelector("#movie");
+const seats = document.querySelectorAll(".row .seat:not(.occupied)");
+const container = document.querySelector(".container");
+const seatCount = document.querySelector("#count");
+const totalPrice = document.querySelector("#price");
+let ticketPrice = +selectMovie.value;
+
+seats.forEach((seat) => {
+  seat.addEventListener("click", () => {
+    seat.classList.toggle("selected");
+
+    const selectedSeat = container.querySelectorAll(".seat.selected");
+    seatCount.innerHTML = selectedSeat.length;
+    totalPrice.textContent = selectedSeat.length * ticketPrice;
+  });
+});
