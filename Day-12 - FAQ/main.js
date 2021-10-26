35f8ab796f9d17a2640093f95db5a0eabbcf87a6
Rafiur Rahman <rrfoundary@gmail.com>
2021-10-26 11:06:36 +0600

    faq - handle button increment using react

--- Day-12 - FAQ/main.js
+++ Day-12 - FAQ/main.js
@@ -1,7 +1,9 @@
-const h1 = document.querySelector("h1");
-const button = document.querySelector("button");
-let number = 0;
-button.addEventListener("click", () => {
-  number++;
-  h1.textContent = number;
-});
+// document.addEventListener("DOMContentLoaded", () => {
+//   const h1 = document.querySelector("h1");
+//   const button = document.querySelector("button");
+//   let number = 0;
+//   button.addEventListener("click", () => {
+//     number++;
+//     h1.textContent = number;
+//   });
+// });
