4c43c0e97cf104efe994f90372727af604412f40
Rafiur Rahman <rrfoundary@gmail.com>
2021-10-26 10:56:33 +0600

    faq - added functionality using js

--- Day-12 - FAQ/main.js
+++ Day-12 - FAQ/main.js
@@ -1,13 +1,7 @@
+const h1 = document.querySelector("h1");
 const button = document.querySelector("button");
-const img = document.querySelector("img");
-button.addEventListener("click", (e) => {
-  var xhr = new XMLHttpRequest();
-  xhr.onreadystatechange = function () {
-    if (xhr.readyState == 4 && xhr.status == 200) {
-      const dog = JSON.parse(xhr.responseText);
-      img.src = dog.message;
-    }
-  };
-  xhr.open("GET", "https://dog.ceo/api/breeds/image/random");
-  xhr.send();
+let number = 0;
+button.addEventListener("click", () => {
+  number++;
+  h1.textContent = number;
 });
