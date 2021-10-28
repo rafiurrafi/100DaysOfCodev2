256068311583dd291b00b0b19cd6d782a28a1ab3
Rafiur Rahman <rrfoundary@gmail.com>
2021-10-28 23:11:53 +0600

    faq - add button for fetch requet

--- Day-12 - FAQ/main.js
+++ Day-12 - FAQ/main.js
@@ -1,9 +1,7 @@
-const price = document.querySelector("#btc-price");
-const xhr = new XMLHttpRequest();
-xhr.onreadystatechange = function () {
-  if (xhr.readyState === 4 && xhr.status === 200) {
-    price.textContent = JSON.parse(xhr.responseText).bpi.EUR.rate;
-  }
-};
-xhr.open("GET", "https://api.coindesk.com/v1/bpi/currentprice.json");
-xhr.send();
+const btn = document.querySelector("button");
+
+btn.addEventListener("click", function () {
+  fetch("https://api.github.com/user/rafiurrafi")
+    .then((res) => res.json())
+    .then((data) => console.log(data));
+});
