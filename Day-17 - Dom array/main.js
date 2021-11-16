e73a02d83711d6a9672bc8cf3dce16fe867beea8
Rafiur Rahman <rrfoundary@gmail.com>
2021-11-16 20:18:23 +0600

    dom - added calculate wealth functionality

--- Day-17 - Dom array/main.js
+++ Day-17 - Dom array/main.js
@@ -1,10 +1,11 @@
 const main = document.querySelector("#main");
 const addUserBtn = document.querySelector("#add-user");
+const doubleBtn = document.querySelector("#double");
 const showMillionariesBtn = document.querySelector("#show-millionaries");
 const sortBtn = document.querySelector("#sort");
 const caleculateWealthBtn = document.querySelector("#calculate-wealth");
 
-const data = [];
+let data = [];
 
 getRandomUser();
 getRandomUser();
@@ -30,6 +31,7 @@ function addData(user) {
 }
 
 function updateDOM(providedData = data) {
+  main.innerHTML = ` <h2><strong>Person</strong> Wealth</h2>`;
   providedData.forEach((item) => {
     const element = document.createElement("div");
     element.classList.add("person");
@@ -43,4 +45,24 @@ function formateMoney(money) {
   return "$" + money + ".00";
 }
 
+function doubleMoney() {
+  data = data.map((user) => ({ ...user, money: user.money * 2 }));
+  updateDOM();
+}
+function sortByIncrese() {
+  data.sort((a, b) => b.money - a.money);
+  updateDOM();
+}
+function showMillionaries() {
+  data = data.filter((user) => user.money > 1000000);
+  updateDOM();
+}
+function calculateTotal() {
+  const total = data.reduce((acc, num) => acc + num.money, 0);
+  console.log(total);
+}
 addUserBtn.addEventListener("click", getRandomUser);
+doubleBtn.addEventListener("click", doubleMoney);
+sortBtn.addEventListener("click", sortByIncrese);
+showMillionariesBtn.addEventListener("click", showMillionaries);
+caleculateWealthBtn.addEventListener("click", calculateTotal);
