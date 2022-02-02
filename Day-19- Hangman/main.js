441e8ae3cbe1d0c189cc6f247c4df664d9c98dbb
Rafiur Rahman <rrfoundary@gmail.com>
2022-02-02 10:22:03 +0600

    d3 - working with svg

--- Day-19- Hangman/main.js
+++ Day-19- Hangman/main.js
@@ -6,35 +6,36 @@ const notification = document.querySelector("#notification-container");
 const finalMessage = document.querySelector("#final-message");
 const figureParts = document.querySelectorAll(".figure-part");
 
-const words = ["Application", "programming", "interface", "apple"];
+const words = ["application", "programming", "interface", "apple"];
 
 let selectedWord = words[Math.floor(Math.random() * words.length)];
 
 const correctedLetters = [];
 const wrongLetters = [];
-
+displayWord();
 // show hidden word
+
 function displayWord() {
   wordEl.innerHTML = `
     ${selectedWord
       .split("")
       .map(
         (letter) =>
-          `<span class="letter">
-          ${correctedLetters.includes(letter) ? letter : ""}
-        </span>`
+          `<span class = "letter">${
+            correctedLetters.includes(letter) ? letter : ""
+          }</span>`
       )
       .join("")}
-    `;
+  `;
   const innerWords = wordEl.innerText.replace(/\n/g, "");
-  console.log(selectedWord, innerWords);
   if (innerWords === selectedWord) {
-    finalMessage.innerHTML = "You've won 😁";
+    finalMessage.innerHTML = "You've won 🤣";
     popup.style.display = "flex";
   }
 }
 
 //keydown letter press
+
 window.addEventListener("keydown", (e) => {
   if (e.keyCode >= 65 && e.keyCode <= 90) {
     const letter = e.key;
@@ -42,38 +43,32 @@ window.addEventListener("keydown", (e) => {
       if (!correctedLetters.includes(letter)) {
         correctedLetters.push(letter);
         displayWord();
-      } else {
-        showNotification();
-      }
+      } else showNotification();
     } else {
       if (!wrongLetters.includes(letter)) {
         wrongLetters.push(letter);
         updateWrongLetterEl();
-      } else {
-        showNotification();
-      }
+      } else showNotification();
     }
   }
 });
-displayWord();
-
 //function update the wrong letters
+
 function updateWrongLetterEl() {
-  wrongLetters.innerHTML = `
+  wordLettersEl.innerHTML = `
     ${wrongLetters.length > 0 ? "<p>Wrong</p>" : ""}
-    ${wrongLetters.map((letter) => "<span>${letter}</span>")}
+    ${wrongLetters.map((letter) => `<span>${letter}</span>`)}
   `;
   figureParts.forEach((part, index) => {
     const errors = wrongLetters.length;
-    if (index < errors) {
-      part.style.display = "block";
-    } else part.style.display = "none";
-  });
+    if (index < errors) part.style.display = "block";
+    else part.style.display = "none";
 
-  if (wrongLetters.length === figureParts.length) {
-    finalMessage.innerHTML = "Unfortunately you've lost 😥";
-    popup.style.display = "flex";
-  }
+    if (wrongLetters.length === figureParts.length) {
+      finalMessage.innerHTML = "Unfortunately, you lost 😭";
+      popup.style.display = "flex";
+    }
+  });
 }
 function showNotification() {
   notification.classList.add("show");
@@ -81,6 +76,14 @@ function showNotification() {
     notification.classList.remove("show");
   }, 2000);
 }
+// playAgainBtn.addEventListener("click", () => {
+//   correctedLetters.splice(0);
+//   wrongLetters.splice(0);
+//   selectedWord = words[Math.floor(Math.random() * words.length)];
+//   displayWord();
+//   updateWrongLetterEl();
+//   popup.style.display = "none";
+// });
 playAgainBtn.addEventListener("click", () => {
   correctedLetters.splice(0);
   wrongLetters.splice(0);
