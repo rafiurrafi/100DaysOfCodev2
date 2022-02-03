70452fd74ffcc68b440297da6e9c058b1272398f
Rafiur Rahman <rrfoundary@gmail.com>
2022-02-03 10:32:45 +0600

    d3 - added the environment for bar-chart

--- Day-13 - form-validator/main.js
+++ Day-13 - form-validator/main.js
@@ -1,47 +1,9 @@
-const button = document.querySelector("button");
-const userInput = document.querySelector("#username");
-const emailInput = document.querySelector("#email");
-const passwordInput = document.querySelector("#password");
-const passwordInput2 = document.querySelector("#password2");
+const form = document.querySelector("form");
+const username = document.querySelector("#username");
+const email = document.querySelector("#email");
+const password = document.querySelector("#password");
+const password2 = document.querySelector("#password2");
 
-button.addEventListener("click", (e) => {
+form.addEventListener("submit", (e) => {
   e.preventDefault();
-  checkRequired([userInput, emailInput, passwordInput, passwordInput2]);
-  checkLength(userInput, 3, 15);
 });
-
-function showError(input, msg) {
-  const parent = input.parentNode;
-  const small = parent.querySelector("small");
-  parent.className = "form-control error";
-  small.textContent = msg;
-  console.log(parent);
-}
-function showSuccess(input) {
-  const parent = input.parentNode;
-  parent.className = "form-control success";
-}
-
-function checkRequired(inputs) {
-  inputs.map((input) => {
-    if (input.value.trim() == "")
-      showError(input, getFieldName(input) + " is empty");
-    else showSuccess(input);
-  });
-}
-function getFieldName(input) {
-  return input.id.charAt(0).toUpperCase() + input.id.slice(1);
-}
-function checkLength(input, min, max) {
-  if (input.value.length < min)
-    showError(
-      input,
-      getFieldName(input) + " should at least " + min + " character"
-    );
-  else if (input.value.length > max)
-    showError(
-      input,
-      getFieldName(input) + " should less than " + max + " character"
-    );
-  else showSuccess(input);
-}
