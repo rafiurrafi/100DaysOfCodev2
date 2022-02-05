3263c28fd22f15d7e12106a45fea88936dfea385
Rafiur Rahman <rrfoundary@gmail.com>
2022-02-05 11:55:53 +0600

    d3 - added cafe barchart of 6 month sheet

--- Day- Ex7 - D3/js/main.js
+++ Day- Ex7 - D3/js/main.js
@@ -1,28 +1,6 @@
 /*
- *    main.js
- *    Mastering Data Visualization with D3.js
- *    5.2 - Looping with intervals
- */
+*    main.js
+*    Mastering Data Visualization with D3.js
+*    Project 1 - Star Break Coffee
+*/
 
-const svg = d3
-  .select("#chart-area")
-  .append("svg")
-  .attr("height", 400)
-  .attr("width", 400);
-
-const data = d3.json("data/buildings.json").then((data) => {
-  data.forEach((d) => {
-    d.height = +d.height;
-  });
-  const y = d3.scaleLinear().domain([0, 828]).range([0, 400]);
-  svg
-    .selectAll("rect")
-    .data(data)
-    .enter()
-    .append("rect")
-    .attr("y", 20)
-    .attr("x", (d, i) => i * 60)
-    .attr("width", 40)
-    .attr("height", (d) => y(d.height))
-    .attr("fill", "grey");
-});
