99ed9933cbf53c35b2111adad27c2ca1d581a4cd
Rafiur Rahman <rrfoundary@gmail.com>
2022-02-14 21:01:15 +0600

    drawing - refactor the timer class into separate file

--- Day- 20 - Drawing animation/main.js
+++ Day- 20 - Drawing animation/main.js
@@ -1,40 +1,14 @@
-class Timer {
-  constructor(durationInput, startButton, pauseButton, callbacks) {
-    this.durationInput = durationInput;
-    this.startButton = startButton;
-    this.pauseButton = pauseButton;
-
-    if (callbacks) {
-      this.onStart = callbacks.onStart;
-    }
-
-    this.startButton.addEventListener("click", this.start);
-    this.pauseButton.addEventListener("click", this.pause);
-  }
-  start = () => {
-    if (this.onStart) this.onStart();
-    this.tick();
-    this.interval = setInterval(this.tick, 1000);
-  };
-  pause = () => {
-    clearInterval(this.interval);
-  };
-  tick() {
-    let timeRemaining = this.durationInput.value - 1;
-    this.durationInput.value = timeRemaining;
-    console.log(timeRemaining);
-    if (this.durationInput.value <= 0) clearInterval(this.interval);
-  }
-}
-
 const durationInput = document.querySelector("#durationInput");
 const startButton = document.querySelector("#startButton");
 const pauseButton = document.querySelector("#pauseButton");
-
 const timer = new Timer(durationInput, startButton, pauseButton, {
   onStart() {
     console.log("Timer started");
   },
-  onTick() {},
-  onComplete() {},
+  onTick() {
+    console.log("Timer ticking...");
+  },
+  onComplete() {
+    console.log("Timer completed");
+  },
 });
--- Day- Ex7 - D3/js/main.js
+++ Day- Ex7 - D3/js/main.js
@@ -2,45 +2,74 @@
  *    main.js
  *    Mastering Data Visualization with D3.js
  *    3.2 - Linear scales
- */
-
-const margin = { bottom: 50, left: 30, right: 10, top: 10 };
-const width = 450 - margin.left - margin.right;
-const height = 500 - margin.top - margin.bottom;
-const svg = d3
-  .select("#chart-area")
-  .append("svg")
-  .attr("width", width + margin.left + margin.right)
-  .attr("height", height + margin.top + margin.bottom);
-
-const g = svg
-  .append("g")
-  .attr("transform", "translate(" + margin.left + "," + margin.right + ")");
-
-d3.json("data/buildings.json").then((data) => {
-  data.forEach((d) => {
-    d.height = +d.height;
-  });
-  const x = d3
-    .scaleBand()
-    .domain([
-      "Burj Khalifa",
+ * "Burj Khalifa",
       "Shanghai Tower",
       "Abraj Al-Bait Clock Tower",
       "Ping An Finance Centre",
       "Lotte World Tower",
-    ])
-    .range([0, 430])
-    .paddingInner(0.2)
-    .paddingOuter(0.2);
-  const y = d3.scaleLinear().domain([0, 900]).range([0, 400]);
-  g.selectAll("rect")
-    .data(data)
-    .enter()
+ */
+const margin = { bottom: 50, top: 10, right: 10, left: 50 };
+const width = 450 - margin.left - margin.right;
+const height = 500 - margin.top - margin.left;
+const svg = d3
+  .select("#chart-area")
+  .append("svg")
+  .attr("width", width + margin.left + margin.right)
+  .attr("height", height + margin.left + margin.right);
+
+const g = svg
+  .append("g")
+  .attr("transform", "translate(" + margin.left + ", " + margin.top + ")");
+
+const x = d3.scaleBand().range([0, width]).paddingInner(0.2).paddingOuter(0.2);
+const y = d3.scaleLinear().range([0, height]);
+
+const xAxisGroup = g
+  .append("g")
+  .attr("class", "x-axis")
+  .attr("transform", "translate(0, " + height + ")");
+// .selectAll("text")
+// .attr("text-anchor", "end")
+// .attr("transform", "rotate(-40)");
+
+const yAxisGroup = g.append("g").attr("class", "y-axis");
+g.append("text")
+  .attr("x", width / 2)
+  .attr("y", height + 140)
+  .attr("text-anchor", "middle")
+  .text("World large tallest buildings");
+d3.json("data/buildings.json").then((data) => {
+  data.forEach((d) => {
+    d.height = +d.height;
+  });
+
+  d3.interval(() => {
+    console.log("Run");
+    update(data);
+  }, 1000);
+});
+function update(data) {
+  x.domain(data.map((d) => d.name));
+  y.domain([0, d3.max(data, (d) => d.height)]);
+  const xAxisCall = d3.axisBottom(x);
+  xAxisGroup.call(xAxisCall);
+
+  const yAxisCall = d3.axisLeft(y);
+  yAxisGroup.call(yAxisCall);
+  const rects = g.selectAll("rect").data(data).enter();
+
+  rects.exit().remove();
+  rects
+    .attr("x", (d) => x(d.name))
+    .attr("y", 20)
+    .attr("height", (d) => y(d.height))
+    .attr("width", x.bandwidth);
+
+  rects
     .append("rect")
     .attr("x", (d) => x(d.name))
-    .attr("y", 40)
+    .attr("y", 20)
     .attr("height", (d) => y(d.height))
     .attr("width", x.bandwidth)
     .attr("fill", "grey");
-});
+}
