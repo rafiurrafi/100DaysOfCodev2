04158f335b1afb77f1221313d817e90ba0a01b95
Rafiur Rahman <rrfoundary@gmail.com>
2022-02-15 20:11:08 +0600

    fighter - added fetch data function using axios

--- Day- 21 - movie fighter/index.js
+++ Day- 21 - movie fighter/index.js
@@ -1,1 +1,10 @@
-console.log('Hi there!');
+async function fetchDate(searchTerm) {
+  const { data } = await axios.get("http://www.omdbapi.com/", {
+    params: {
+      apikey: "22ce8a24",
+      s: searchTerm,
+    },
+  });
+  console.log(data);
+}
+fetchDate();
