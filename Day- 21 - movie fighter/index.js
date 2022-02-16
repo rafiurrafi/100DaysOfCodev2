c08f5e777305887321685ff4e86b048004c016fc
Rafiur Rahman <rrfoundary@gmail.com>
2022-02-17 00:10:41 +0600

    fighter - added dropdown using js

--- Day- 21 - movie fighter/index.js
+++ Day- 21 - movie fighter/index.js
@@ -9,18 +9,37 @@ async function fetchDate(searchTerm) {
   return data.Search;
 }
 
+const root = document.querySelector(".autocomplete");
+
+root.innerHTML = `
+  <label><b>Search For a Movie </b></label>
+  <input class = "input" />
+  <div class = "dropdown">
+    <div class = "dropdown-menu">
+      <div class = "dropdown-content results"></div>
+    </div>
+  </div>
+`;
+const input = document.querySelector(".input");
+const dropdown = document.querySelector(".dropdown");
+const resultWrapper = document.querySelector(".results");
+
 const onInput = async (event) => {
   const movies = await fetchDate(event.target.value);
   //   if (!movies) return;
+  resultWrapper.innerHTML = "";
+  dropdown.classList.add("is-active");
   for (let movie of movies) {
-    const div = document.createElement("div");
-    div.innerHTML = `
-        <img src = "${movie.Poster}" />
+    const option = document.createElement("a");
+    option.classList.add("dropdown-item");
+    option.innerHTML = `
+        <img src = "${movie.Poster === "N/A" ? "" : movie.Poster}" />
         <h1>${movie.Title}</h1>
       `;
-    document.querySelector("#target").appendChild(div);
+    resultWrapper.appendChild(option);
   }
 };
-const firstMovieInput = document.querySelector("#firstMovieInput");
+
+const firstMovieInput = document.querySelector(".input");
 
 firstMovieInput.addEventListener("input", debounce(onInput));
