9f801a0d4d5910662c40e5e5049ad0eac97aa5a9
Rafiur Rahman <rrfoundary@gmail.com>
2022-06-26 06:45:50 +0600

    added mock functions for get fetch

--- Day- 24- Tesing/script-async.test.js
+++ Day- 24- Tesing/script-async.test.js
@@ -8,3 +8,27 @@ it("calls swapi to get people", (done) => {
     done();
   });
 });
+it("can test mock function also", () => {
+  const mockFunction = jest.fn().mockReturnValue(
+    Promise.resolve({
+      json: () =>
+        Promise.resolve({
+          count: 87,
+          reults: [0, 1, 2, 3, 4, 5],
+        }),
+    })
+  );
+
+  const mc = jest.fn().mockReturnValue(
+    Promise.resolve({
+      json: () =>
+        Promise.resolve({
+          count: 87,
+          result: [1, 3, 2, 3, 12, 1211, 1],
+        }),
+    })
+  );
+  return swapi.getPeople(mockFunction).then((data) => {
+    expect(mockFunction.mock.calls.length).toBe(1);
+  });
+});
