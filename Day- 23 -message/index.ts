6d7d35388cb51e3494f26063f7e8ba9dcd3fdefe
Rafiur Rahman <rrfoundary@gmail.com>
2022-07-23 01:55:21 +0600

    added build in typescript for js

--- Day- 23 -message/index.ts
+++ Day- 23 -message/index.ts
@@ -1,1 +1,9 @@
-console.log("Hello");
+let sales: number = 1234_3453;
+let courses: string = "Typescript";
+const isPublished: boolean = false;
+
+let level;
+
+function render(document: any) {
+  console.log(document);
+}
