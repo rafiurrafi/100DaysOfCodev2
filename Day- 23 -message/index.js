67c24f31ad5563c00cbf75152b8592d5d015a1a3
Rafiur Rahman <rrfoundary@gmail.com>
2022-07-25 14:27:29 +0600

    added random guess input function

--- Day- 23 -message/index.js
+++ Day- 23 -message/index.js
@@ -1,4 +1,16 @@
 "use strict";
-const user = [1, "false"];
-user.push(23);
-console.log(user);
+class Account {
+    constructor(id, owner, amount) {
+        this.id = id;
+        this.owner = owner;
+        this.balance = amount;
+    }
+    deposite(amount) {
+        if (amount <= 0)
+            throw new Error("Invalid amount " + amount);
+        this.balance += amount;
+    }
+}
+const account = new Account(1, "Honu", 0);
+account.deposite(100);
+console.log(account);
--- Day- 23 -message/index.ts
+++ Day- 23 -message/index.ts
@@ -1,18 +1,42 @@
 class Account {
   id: number;
   owner: string;
-  balance: number;
+  private _balance: number;
   constructor(id: number, owner: string, amount: number) {
     this.id = id;
     this.owner = owner;
-    this.balance = amount;
+    this._balance = amount;
   }
   deposite(amount: number): void {
     if (amount <= 0) throw new Error("Invalid amount " + amount);
-    this.balance += amount;
+    //transations
+
+    this._balance += amount;
+  }
+  get balance(): number {
+    return this._balance;
+  }
+  set balance(amount: number) {
+    this._balance = amount;
   }
 }
 
+class Uber {
+  private static activeRides: number = 0;
+  start() {
+    Uber.activeRides++;
+  }
+  stop() {
+    Uber.activeRides--;
+  }
+}
+var ride1 = new Uber();
+ride1.start();
+var ride2 = new Uber();
+ride2.start();
+console.log();
+
 const account = new Account(1, "Honu", 0);
 account.deposite(100);
-console.log(account);
+console.log(account.balance);
+console.log((account.balance = 4));
