67de527f6253736ceed152e6638417b1d4d98ce0
Rafiur Rahman <rrfoundary@gmail.com>
2022-08-30 10:56:30 +0600

    added starter for typescript

--- Day- 23 -message/index.js
+++
@@ -1,8 +0,0 @@
-const googleDatabase = ["nokia.com", "samsung.com", "readme.com", "realme.com"];
-const googleSearch = (searchInput, db) => {
-  const matches = db.filter((website) => website.includes(searchInput));
-  return matches.length > 3 ? matches.splice(0, 3) : matches;
-};
-
-console.log(googleSearch("rea"));
-module.exports = googleDatabase;
