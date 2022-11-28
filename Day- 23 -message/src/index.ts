8ea3b25bed1e720e79ca1509845ee47a0a070253
Rafiur Rahman <rrfoundary@gmail.com>
2022-11-28 21:18:15 +0600

    learning how to use tailwind

--- Day- 23 -message/src/index.ts
+++ Day- 23 -message/src/index.ts
@@ -1,43 +1,5 @@
-const button = document.querySelector("#btn")! as HTMLButtonElement;
-const form = document.querySelector("#form");
-const list = document.querySelector("#list");
-const input = document.querySelector("#input") as HTMLInputElement;
-
-type Todo = {
-  text: string;
-  completed: boolean;
-};
-function getTodos() {
-  const todos = localStorage.getItem("todos");
-  if (!todos) return [];
-  return JSON.parse(todos);
+function add(a: number, b: number): number {
+  return a + b;
 }
-const todos: Todo[] = getTodos() || [];
-
-function createTodo(todo: Todo) {
-  const item = document.createElement("li");
-  item.innerHTML = todo.text;
-  const checkbox = document.createElement("input");
-  checkbox.type = "checkbox";
-  checkbox.checked = todo.completed;
-  checkbox.addEventListener("change", (e) => {
-    todo.completed = checkbox.checked;
-    localStorage.setItem("todos", JSON.stringify(todos));
-  });
-  item.appendChild(checkbox);
-  list?.appendChild(item);
-}
-
-todos.forEach(createTodo);
-
-(<HTMLFormElement>form)?.addEventListener("submit", (e: SubmitEvent) => {
-  e.preventDefault();
-  const newTodo: Todo = {
-    text: input.value,
-    completed: false,
-  };
-  createTodo(newTodo);
-  todos.push(newTodo);
-  localStorage.setItem("todos", JSON.stringify(todos));
-  input.value = "";
-});
+add(12, 2);
+add("one", 1);
