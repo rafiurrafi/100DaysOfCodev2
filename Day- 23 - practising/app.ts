d1ba3f15ea28948170b60a6e4bae9d625a2029f5
Rafiur Rahman <rrfoundary@gmail.com>
2023-01-07 23:51:28 +0600

    added tailwind config

--- Day- 23 - practising/app.ts
+++
@@ -1,7 +0,0 @@
-const userName: Array<string> = [];
-
-const promise : Promise<string> = new Promise((resolve, reject) => {
-  setTimeout(() => {
-    resolve("Hello");
-  }, 1000);
-});
--- Day- 23 -message/src/index.ts
+++
@@ -1,5 +0,0 @@
-function add(a: number, b: number): number {
-  return a + b;
-}
-add(12, 2);
-add("one", 1);
