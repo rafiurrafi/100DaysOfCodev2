b81c5f19e64d61910e79c2781d1ff5aa13e46bee
Rafiur Rahman <rrfoundary@gmail.com>
2023-01-17 21:04:05 +0600

    added basic setup for tailwind

--- Day- 23 -message/tailwind.config.js
+++ Day- 23 -message/tailwind.config.js
@@ -1,8 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./*/*.html"],
+  // content: [],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
