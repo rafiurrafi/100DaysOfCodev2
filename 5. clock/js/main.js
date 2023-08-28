04b86dd2e73bb7398cb230576cbd120b77a2277d
Rafiur Rahman <rrfoundary@gmail.com>
2023-08-28 13:58:52 +0600

    populate select options and value

--- 5. clock/js/main.js
+++ 5. clock/js/main.js
@@ -1,17 +1,43 @@
 const clock = document.querySelector(".clock");
-
+const selects = document.querySelectorAll("select");
+const hourSelect = document.querySelector("#hour-select");
+const minuteSelect = document.querySelector("#minute-select");
+const amSelect = document.querySelector("#am-select");
 setInterval(() => {
   let currentTime = new Date();
   const hours = currentTime.getHours();
   const minutes = currentTime.getMinutes();
   const seconds = currentTime.getSeconds();
-  console.log(hours);
-  const formatedTime = formatTime(hours, minutes, seconds);
+
+  const formatedTime = `${formatTime(hours)} : ${formatTime(
+    minutes
+  )} : ${formatTime(seconds)}`;
   clock.textContent = formatedTime;
 }, 1000);
 
-function formatTime(hours, minutes, seconds) {
-  return `${hours.toString().padStart(2, 0)} : ${minutes
-    .toString()
-    .padStart(2, 0)} : ${seconds.toString().padStart(2, 0)}`;
+function formatTime(time) {
+  return `${time.toString().padStart(2, 0)}`;
 }
+document.addEventListener("DOMContentLoaded", () => {
+  for (let i = 1; i <= 12; i++) {
+    const value = i.toString().padStart(2, "0");
+    const option = document.createElement("option");
+    option.value = value;
+    option.textContent = value;
+    hourSelect.appendChild(option);
+  }
+  for (let i = 0; i < 60; i++) {
+    const value = i.toString().padStart(2, "0");
+    const option = document.createElement("option");
+    option.value = value;
+    option.textContent = value;
+    minuteSelect.appendChild(option);
+  }
+  for (let i = 1; i <= 2; i++) {
+    const value = i === 1 ? "AM" : "PM";
+    const option = document.createElement("option");
+    option.value = value;
+    option.textContent = value;
+    amSelect.appendChild(option);
+  }
+});
