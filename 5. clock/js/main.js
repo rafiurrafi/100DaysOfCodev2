e65712b67ca9e9117ddc75ecff90381e9360aa65
Rafiur Rahman <rrfoundary@gmail.com>
2023-08-28 14:20:29 +0600

    added set alarm function

--- 5. clock/js/main.js
+++ 5. clock/js/main.js
@@ -3,6 +3,7 @@ const selects = document.querySelectorAll("select");
 const hourSelect = document.querySelector("#hour-select");
 const minuteSelect = document.querySelector("#minute-select");
 const amSelect = document.querySelector("#am-select");
+const addBtn = document.querySelector("button");
 setInterval(() => {
   let currentTime = new Date();
   const hours = currentTime.getHours();
@@ -12,11 +13,21 @@ setInterval(() => {
   const formatedTime = `${formatTime(hours)} : ${formatTime(
     minutes
   )} : ${formatTime(seconds)}`;
+
   clock.textContent = formatedTime;
 }, 1000);
-
 function formatTime(time) {
   return `${time.toString().padStart(2, 0)}`;
+}
+function setAlarm() {
+  let selectetHour = hourSelect.value;
+  const selectetMinute = minuteSelect.value;
+  const selectetMeridian = amSelect.value;
+  if (selectetMeridian === "PM") selectetHour = +selectetHour + 12;
+  const createTime = `${selectetHour} : ${selectetMinute}`;
+  const runningTimes = clock.textContent.split(":");
+  const time = `${runningTimes[0]} : ${runningTimes[1]}`;
+  console.log(createTime, time);
 }
 document.addEventListener("DOMContentLoaded", () => {
   for (let i = 1; i <= 12; i++) {
@@ -39,5 +50,11 @@ document.addEventListener("DOMContentLoaded", () => {
     option.value = value;
     option.textContent = value;
     amSelect.appendChild(option);
+  }
+});
+addBtn.addEventListener("click", function () {
+  if (this.textContent == "Add alarm") {
+    // this.textContent = "Reset";
+    setAlarm();
   }
 });
