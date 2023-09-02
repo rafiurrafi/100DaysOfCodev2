3c603cfed0d91aa9296d7f0d3a7745fdad72d015
Rafiur Rahman <rrfoundary@gmail.com>
2023-09-02 17:43:36 +0600

    added 8 game functionality

--- 1. accordion/main.js
+++ 1. accordion/main.js
@@ -1,3 +1,33 @@
-const list = document.querySelector("ul");
+const answers = [
+  "yes,sure",
+  "no man",
+  "Exactly found ",
+  "Good question",
+  "Beautiful eys",
+];
+const button = document.querySelector("button");
+const input = document.querySelector("input");
+const answerEl = document.querySelector("#message");
+function getRandomNumber() {
+  return Math.floor(Math.random() * answers.length);
+}
+function getRandomAnswer(answers) {
+  const randomNumber = getRandomNumber();
+  return answers[randomNumber];
+}
+input.addEventListener("keydown", function () {
+  answerEl.textContent = "";
+});
+button.addEventListener("click", function () {
+  // check input value
+  if (input.value.length < 5) {
+    answerEl.textContent = "Ask valid ques";
+    return;
+  }
+  // get random value
+  const answer = getRandomAnswer(answers);
 
-console.log(list.closest("body"));
+  //show to screen
+  answerEl.textContent = answer;
+  input.value = "";
+});
