344bd5e72ac6d88f93b7f6c6a9f03bdb59422a7f
Rafiur Rahman <rrfoundary@gmail.com>
2023-09-02 19:44:58 +0600

    added check log functionality

--- 1. accordion/main.js
+++ 1. accordion/main.js
@@ -1,33 +1,44 @@
-const answers = [
-  "yes,sure",
-  "no man",
-  "Exactly found ",
-  "Good question",
-  "Beautiful eys",
-];
+const gameArea = document.querySelector(".game");
 const button = document.querySelector("button");
-const input = document.querySelector("input");
-const answerEl = document.querySelector("#message");
-function getRandomNumber() {
-  return Math.floor(Math.random() * answers.length);
-}
-function getRandomAnswer(answers) {
-  const randomNumber = getRandomNumber();
-  return answers[randomNumber];
-}
-input.addEventListener("keydown", function () {
-  answerEl.textContent = "";
-});
-button.addEventListener("click", function () {
-  // check input value
-  if (input.value.length < 5) {
-    answerEl.textContent = "Ask valid ques";
-    return;
-  }
-  // get random value
-  const answer = getRandomAnswer(answers);
+let gameStart = false,
+  winCondition = 0;
 
-  //show to screen
-  answerEl.textContent = answer;
-  input.value = "";
+function inputMaker() {
+  for (let i = 0; i < 4; i++) {
+    const input = document.createElement("input");
+    input.min = 0;
+    input.max = 9;
+    input.size = 1;
+    input.order = 0;
+    input.correct = Math.floor(Math.random() * 10);
+    input.value = input.correct;
+    input.style.width = "40px";
+    input.style.height = "40px";
+    input.style.fontSize = "20px";
+    input.style.textAlign = "center";
+    input.setAttribute("type", "number");
+    gameArea.appendChild(input);
+  }
+}
+
+button.addEventListener("click", () => {
+  if (!gameStart) {
+    gameStart = true;
+    inputMaker();
+    button.textContent = "Check number";
+  } else {
+    const numbers = document.querySelectorAll("input");
+    numbers.forEach((number) => {
+      if (number.value == number.correct) {
+        number.style.backgroundColor = "green";
+        number.style.color = "white";
+        winCondition++;
+      } else {
+        let color = number.correct < number.value ? "blue" : "red";
+        number.style.backgroundColor = color;
+        number.style.color = "white";
+      }
+      if (winCondition === numbers.length) console.log("Win");
+    });
+  }
 });
