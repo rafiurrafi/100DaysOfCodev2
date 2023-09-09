93bcb8bb48a84e9e5ddd804746e6d430dfdac797
Rafiur Rahman <rrfoundary@gmail.com>
2023-09-09 14:17:11 +0600

    added time functionality

--- 1. accordion/main.js
+++ 1. accordion/main.js
@@ -6,19 +6,26 @@ createElement();
 function createElement() {
   const box = document.createElement("div");
   box.classList.add("box");
-  box.style.height = 100 + "px";
-  box.style.top = 100 + "px";
-  box.style.width = 100 + "px";
-  box.style.left = 100 + "px";
-  box.style.borderRadius = 10 + "px";
+  box.style.height = getRandomValue(70) + 50 + "px";
+  box.style.top = getRandomValue(70) + 50 + "px";
+  box.style.width = getRandomValue(70) + 50 + "px";
+  box.style.left = getRandomValue(70) + 50 + "px";
+  box.style.borderRadius = getRandomValue(40) + "px";
   box.style.position = "absolute";
   box.style.backgroundColor = "crimson";
+  box.time = new Date().getTime();
   box.addEventListener("click", updateBox);
   document.body.appendChild(box);
 }
+function getRandomValue(limit) {
+  return Math.floor(Math.random() * limit);
+}
 function updateBox(e) {
   document.body.removeChild(e.target);
+  const endTime = new Date().getTime();
+  const duration = (endTime - e.target.time) / 1000;
+  console.log(duration);
   setTimeout(() => {
     createElement();
-  }, 500);
+  }, getRandomValue(3000));
 }
